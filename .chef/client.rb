b9b4971c261dc0cb348261af002ef0a6783ef622
r-goto <https://gl.creationline.com/r-goto>
2021-04-15 15:03:53 +0900

    Added policyfiles and removed terraform destroy to be secure.

--- .chef/client.rb
+++ .chef/client.rb
@@ -1,7 +1,7 @@
 # node_name "node01-web"
 chef_license 'accept'
 chef_server_url 'https://chef-automate.creationline.com/organizations/first-org'
-policy_group 'staging'
+policy_group 'aws'
 policy_name 'web-server'
 log_location STDOUT
 validation_key '/etc/chef/first-org-validator.pem'
---
+++ policyfiles/web-server.rb
@@ -0,0 +1,31 @@
+# Policyfile.rb - Describe how you want Chef Infra Client to build your system.
+#
+# For more information on the Policyfile feature, visit
+# https://docs.chef.io/policyfile/
+
+name 'web-server'
+default_source :chef_server, 'https://chef-automate.creationline.com/organizations/first-org'
+run_list 'bootstrap_a_node::default', 'nginx_setup::default', 'ssh_setup::default'
+cookbook 'bootstrap_a_node', '1.0.0'
+cookbook 'nginx_setup', '1.0.0'
+cookbook 'ssh_setup', '1.0.0'
+
+##########
+# Attributes
+##########
+
+# Specify CCR interval and splay
+override['chef_client']['interval'] = 86400 # 24H
+override['chef_client']['splay'] = 0
+
+# Specify Policy name & Policy group
+override['bootstrap_a_node']['policy_name'] = 'web-server'
+override['bootstrap_a_node']['policy_group'] = 'aws'
+
+# Specify chef-client version
+override['bootstrap_a_node']['chef_client']['version'] = '16.11.7'
+
+# Disable Slack Notification that's set only for the initial CCR
+override['chef_client']['handler']['slack']['enabled'] = false
+# Specify node Time Zone
+override['bootstrap_a_node']['timezone'] = 'Asia/Tokyo'
