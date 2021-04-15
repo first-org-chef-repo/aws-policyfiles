a3633e6da0de6695db45f5ecba49423f178307ae
r-goto <https://gl.creationline.com/r-goto>
2021-04-15 17:07:43 +0900

    Removed outputs. use knife instead for ip.

--- main.tf
+++ main.tf
@@ -68,8 +68,3 @@ resource "aws_instance" "instance" {
     }
   }
 }
-
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.instance.*.public_ip
-}
