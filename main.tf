ad73ca1d77dd3f23083d2f62df8572df969e811b
Ryuji Goto <g.ryuji0510@gmail.com>
2021-04-21 20:04:11 +0900

    Removed file transfer
    .chef directory is now embedded into aws ami so no need of transfer of the directory during terraform apply.

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 resource "aws_instance" "instance" {
   count         = 5
-  ami           = "ami-04e9946bff41abb20"
+  ami           = "ami-017c8fd4ee2157977"
   instance_type = "t2.large"
   vpc_security_group_ids = [
     "sg-0803dfbfc0c77a2f4",
@@ -24,19 +24,6 @@ resource "aws_instance" "instance" {
     Owner = "r-goto"
     Name = format("chef-terraform-demo-node%02d", count.index + 1)
     Project = "chef-demo"
-  }
-
-  provisioner "file" {
-    source      = ".chef/"
-    destination = "/etc/chef"
-
-    connection {
-      host = self.public_ip
-      user = "ec2-user"
-      type = "ssh"
-      private_key = file("/home/r-goto/r-goto_aws-osaka.pem")
-      timeout = "10m"
-    }
   }
 
   provisioner "remote-exec" {
