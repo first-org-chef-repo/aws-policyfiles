cef4544741f211b8082f2e8c3555cf4f08be2eb4
r-goto <https://github.com/r-goto>
2021-05-01 17:32:48 +0900

    ?

--- policyfiles/db-server.rb
+++ policyfiles/db-server.rb
@@ -22,7 +22,7 @@ override['node_setup']['policy_name'] = 'db-server'
 override['node_setup']['policy_group'] = 'aws'
 
 # Specify chef-client version
-override['node_setup']['chef_client']['version'] = '16.11.7'
+override['node_setup']['chef_client']['version'] = '17.0.242'
 override['node_setup']['chef_client']['channel'] = 'stable'
 
 # Disable Slack Notification that's set only for the initial CCR
--- policyfiles/web-server.rb
+++ policyfiles/web-server.rb
@@ -23,7 +23,7 @@ override['node_setup']['policy_name'] = 'web-server'
 override['node_setup']['policy_group'] = 'aws'
 
 # Specify chef-client version
-override['node_setup']['chef_client']['version'] = '16.11.7'
+override['node_setup']['chef_client']['version'] = '17.0.242'
 override['node_setup']['chef_client']['channel'] = 'stable'
 
 # Disable Slack Notification that's set only for the initial CCR
