966b8dcd7abd1efaf4aaa32bd143980056b807a7
Ryuji Goto <g.ryuji0510@gmail.com>
2021-05-01 17:40:15 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "instance" {
-  count         = 1
+  count         = 5
   ami           = "ami-0795e38244a3c27d6"
   instance_type = "t2.large"
   vpc_security_group_ids = [
